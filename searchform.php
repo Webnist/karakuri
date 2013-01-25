1016e03db7163e8274b912eab4abcfb82e60ac92
Webnist <webnist@webnist.org>
2013-01-25 13:16:38 +0900

    no message

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'karakuri' ); ?>" x-webkit-speech>
-	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search' ); ?>">
-</form>
