86efc347d67ec674305ac333202d732a24cb5d5d
Webnist <webnist@webnist.org>
2013-02-03 18:32:22 +0900

    Karakuri 0.7.1.0

--- comments.php
+++ comments.php
@@ -9,21 +9,21 @@ endif;
 
 <?php if ( have_comments() && $post->comment_count > 0 ) : ?>
 
+	<h2 class="commentlist-title"><?php _e( 'Comment List', 'karakuri' ); ?></h2>
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through  ?>
 		<nav id="comment-nav-above">
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'karakuri' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'karakuri' ) ); ?></div>
+			<p class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'karakuri' ) ); ?></p>
+			<p class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'karakuri' ) ); ?></p>
 		</nav>
 	<?php endif; // check for comment navigation  ?>
-	<h2 class="commentlist-title"><?php _e( 'Comment List', 'karakuri' ); ?></h2>
 	<ol class="commentlist">
 		<?php wp_list_comments( array( 'callback' => 'karakuri_comment', 'style' => 'ol' ) ); ?>
 	</ol>
 
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through  ?>
 		<nav id="comment-nav-below">
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'karakuri' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'karakuri' ) ); ?></div>
+			<p class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'karakuri' ) ); ?></p>
+			<p class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'karakuri' ) ); ?></p>
 		</nav>
 	<?php endif; // check for comment navigation  ?>
 
