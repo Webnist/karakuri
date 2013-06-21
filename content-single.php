9cbacabb8277e04783460725e194e47242b1469d
Webnist <webnist@webnist.org>
2013-06-21 11:31:08 +0900

    Change prefixed

--- content-single.php
+++ content-single.php
@@ -7,13 +7,13 @@
 		<?php karakuri_link_pages(); ?>
 	</section><!-- .entry-content -->
 	<footer class="entry-meta">
-		<?php entry_thumbnail(); ?>
-		<?php entry_data(); ?>
-		<?php entry_terms(); ?>
-		<?php entry_terms( 'post_tag' ); ?>
-		<?php entry_author(); ?>
-		<?php entry_comments(); ?>
-		<?php entry_more_link(); ?>
-		<?php entry_edit_post_link(); ?>
+		<?php karakuri_entry_thumbnail(); ?>
+		<?php karakuri_entry_data(); ?>
+		<?php karakuri_entry_terms(); ?>
+		<?php karakuri_entry_terms( 'post_tag' ); ?>
+		<?php karakuri_entry_author(); ?>
+		<?php karakuri_entry_comments(); ?>
+		<?php karakuri_entry_more_link(); ?>
+		<?php karakuri_entry_edit_post_link(); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
