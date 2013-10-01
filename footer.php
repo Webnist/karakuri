ef78be8b3e38eac47b325c1d101ff13545dd6da1
Webnist <webnist@webnist.org>
2013-10-01 18:00:39 +0900

    add do_action

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
+				<?php do_action( 'after_karakuri_main' ); ?>
 			</div><!-- #main -->
 			<?php get_sidebar(); ?>
 			<footer id="colophon" role="contentinfo">
-				<div id="in-footer">
-					<?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
-					<nav id="footer-nav" class="footer-navigation" role="navigation">
-						<h3><?php _e( 'Footer menu', 'karakuri' ); ?></h3>
-						<?php karakuri_footer_nav(); ?>
-					</nav><!-- #footer-nav -->
-					<?php endif; ?>
-					<?php karakuri_copyright(); ?>
-				</div><!-- #in-footer -->
+				<?php do_action( 'before_karakuri_footer' ); ?>
+				<?php if ( has_nav_menu( 'footer_menu' ) ) : ?>
+				<nav id="footer-nav" class="footer-navigation" role="navigation">
+					<h3><?php _e( 'Footer menu', 'karakuri' ); ?></h3>
+					<?php karakuri_footer_nav(); ?>
+				</nav><!-- #footer-nav -->
+				<?php endif; ?>
+				<?php karakuri_copyright(); ?>
+				<?php do_action( 'after_karakuri_footer' ); ?>
 			</footer><!-- #colophon -->
+			<?php do_action( 'after_karakuri_page' ); ?>
 		</div><!-- #page -->
 		<?php wp_footer(); ?>
 	</body>
--- header.php
+++ header.php
@@ -8,21 +8,26 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<?php do_action( 'karakuri_body_before' ); ?>
+		<?php do_action( 'before_karakuri_body' ); ?>
 		<div id="page" class="hfeed site">
+			<?php do_action( 'before_karakuri_page' ); ?>
 			<header id="masthead" class="site-header" role="banner">
-				<hgroup>
+				<?php do_action( 'before_karakuri_header' ); ?>
+				<div id="site-meta">
 					<h1 id="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-				</hgroup>
+				</div>
 				<div id="header-search-box">
 					<?php get_search_form(); ?>
 				</div><!-- #header-search-box -->
-				<nav id="main-nav" class="header-navigation" role="navigation">
+				<nav id="global-nav" class="header-navigation" role="navigation">
 					<h3 class="menu-toggle"><span><?php esc_html( _e( 'Main menu', 'karakuri' ) ); ?></span></h3>
 					<a class="assistive-text" href="#content" title="Skip to content"><span>Skip to content</span></a>
-					<?php karakuri_main_nav(); ?>
+					<?php karakuri_global_nav(); ?>
 				</nav><!-- #main-nav -->
+				<?php do_action( 'after_karakuri_header' ); ?>
+				<?php karakuri_main_img(); ?>
 			</header><!-- #masthead -->
-			<?php karakuri_main_img(); ?>
+			<?php do_action( 'karakuri_header_after' ); ?>
 			<div id="main" class="wrapper">
+				<?php do_action( 'before_karakuri_main' ); ?>
